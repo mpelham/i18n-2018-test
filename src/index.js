e728907c08f0531898f7b1033720e170a50015c5
Matthew Pelham <pelham@smash.gg>
2018-12-12 10:16:43 -0800

    Switch to Common.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import Foo from './Foo/Foo.en.json';
 import querystring from 'querystring';
 
 function locLoader(language, pathToJSON) {
-    return import(`../${language}/${pathToJSON}`);
+    return import(`../${language}/${pathToJSON}`).then((resp) => {
+        console.log('sup', resp);
+        if (document) {
+            document.getElementById('root').innerHTML = JSON.stringify(resp.default);
+        }
+    });
 }
 
 const isClient = typeof window !== 'undefined';
 const qsObj = isClient ? querystring.parse(window.location.search.substring(1)) : {};
 const lang = qsObj.lang || 'fr';
 
-const loc = locLoader(lang, 'src/Foo/Foo.en.json');
+const loc = locLoader(lang, 'Common.en.json');
 console.log(loc);
